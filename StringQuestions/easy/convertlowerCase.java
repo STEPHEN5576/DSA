8db4976b041b104b355785b9990a7a53a17f5566
STEPHEN5576 <116451839+STEPHEN5576@users.noreply.github.com>
2024-03-19 18:08:55 +0530

    DSA

---
+++ StringQuestions/easy/convertlowerCase.java
@@ -0,0 +1,10 @@
+package StringQuestions.easy;
+
+public class convertlowerCase {
+    public static void main(String[] args) {
+        String str = "STEPHEN";
+        str = str.toLowerCase();
+        str = str.toUpperCase();
+        System.out.println(str);
+    }
+}
