8b035e674f2cf296262538e760b594c74739f924
STEPHEN5576 <116451839+STEPHEN5576@users.noreply.github.com>
2024-03-20 12:11:05 +0530

    DSA

---
+++ StringQuestions/easy/CheckIndex.java
@@ -0,0 +1,19 @@
+package StringQuestions.easy;
+
+public class CheckIndex {
+    public static void main(String[] args) {
+        String str = "Hello, World";
+        int index = 66;
+
+        char result = check(str, index);
+        System.out.println(result);
+    }
+    public static char check(String str, int index){
+        if(index >= 0 && index < str.length()){
+            return str.charAt(index);
+        }
+        else{
+            return '\0';
+        }
+    }
+}
