c3aab4ef138f075d1a8d6968803beab9d352e5e9
STEPHEN5576 <116451839+STEPHEN5576@users.noreply.github.com>
2024-03-20 12:36:37 +0530

    DSA

---
+++ StringQuestions/easy/append/ConcantenatetwoStrings.java
@@ -0,0 +1,10 @@
+package StringQuestions.easy.append;
+
+public class ConcantenatetwoStrings {
+    public static void main(String[] args) {
+        StringBuilder str = new StringBuilder();
+        str.append("Hello");
+        str.append(", World");
+        System.out.println(str);
+    }
+}
