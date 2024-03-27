91ccdb45b37526cea9ee92000b37dd2b0cabe366
STEPHEN5576 <116451839+STEPHEN5576@users.noreply.github.com>
2024-03-27 13:02:00 +0530

    DSA

--- StringQuestions/easy/concatination/q1.java
+++ StringQuestions/easy/concatination/q1.java
@@ -1,11 +1,31 @@
 package StringQuestions.easy.concatination;
-
+import java.util.Scanner;
 public class q1 {
     public static void main(String[] args) {
-        String str = "Hello";
-        String str1 = "World";
+        Scanner sc= new Scanner(System.in);
+        String str = sc.nextLine();
+        String str1 = sc.nextLine();
         String result = str.concat(str1);
         System.out.println(result);
+        int count  =0;
+        for (int i = 0; i < str.length(); i++) {
+            if(str.length() != str1.length()){
+                System.out.println("the string are not the same");
+                break;
+            }
+            char s = str.toLowerCase().charAt(i);
+            char d = str1.toLowerCase().charAt(i);
+            if(s == d){
+                count++;
+            }
+        }
+        if(count == str.length()){
+            System.out.println("The String are the same");
+        }
 
+        // isEmtpy
+        if(str.length() == 0 || str.charAt(0) == ' '){
+            System.out.println("No Strings available");
+        }
     }
 }
