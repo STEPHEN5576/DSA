43f66c7f8a4af7b4800f7f3f75428f9c2678fa98
STEPHEN5576 <116451839+STEPHEN5576@users.noreply.github.com>
2024-04-11 16:23:51 +0530

    DSA

--- WhileLoop/easy/reversethenum.java
+++ WhileLoop/easy/reversethenum.java
@@ -1,4 +1,15 @@
 package WhileLoop.easy;
 
 public class reversethenum {
+    public static void main(String[] args) {
+        int num = 4325;
+        int reversed = 0;
+        while(num > 0){
+            int number = num % 10;
+            reversed = reversed *10 + number;
+            num /= 10;
+        }
+        System.out.println(reversed);
+
+    }
 }
