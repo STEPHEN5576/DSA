4bfca56a67e8a1ea3fedd3d1c899b2f7ce028a56
STEPHEN5576 <116451839+STEPHEN5576@users.noreply.github.com>
2024-04-11 16:11:37 +0530

    DSA

---
+++ WhileLoop/easy/printnumbers.java
@@ -0,0 +1,12 @@
+package WhileLoop.easy;
+
+public class printnumbers {
+    public static void main(String args[]){
+        int num = 1;
+        while(num < 11){
+            System.out.println(num);
+            num++;
+        }
+
+    }
+}
